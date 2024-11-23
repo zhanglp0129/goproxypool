c45f547a6ffff0c66ca708664d82b7eea7527336
zhanglp0129 <zhanglp0129@gmail.com>
2024-11-23 13:50:10 +0800

    优化持久化存储

--- storage/sqlite/sql/init.sql
+++ storage/sqlite/sql/init.sql
@@ -1,19 +1,27 @@
 -- 数据库初始化脚本
 
 -- 代理地址表
-create table proxy_address (
+create table if not exists proxy_addresses (
     -- 主键
     id integer primary key autoincrement,
     -- ip地址，可以是ipv4和ipv6
     ip text not null,
     -- 端口
     port integer not null,
-    -- 协议，一般为http或socks
+    -- 协议
     protocol text not null,
     -- 上一次检测可用性的时间，为纳秒级时间戳
-    detect_time integer null,
+    detect_time integer default 0,
     -- 检测失败次数
     failure_number integer default 0,
+    -- 上次使用的时间，为纳秒级时间戳
+    used_time integer default 0,
     -- 禁止重复ip 端口 协议
     unique(ip, port, protocol)
 );
+
+-- 为检测时间字段添加索引
+create index if not exists idx_detect_time on proxy_addresses(detect_time);
+
+-- 为使用时间创建索引
+create index if not exists idx_used_time on proxy_addresses(used_time);
--- storage/storage.go
+++ storage/storage.go
@@ -2,14 +2,14 @@ package storage
 
 // Storage 代理地址持久化存储的接口
 type Storage interface {
-	// InsertProxyAddress 插入一个代理地址。当ID为0时，应当自动生成id
+	// InsertProxyAddress 插入一个代理地址。ID应当为0，由系统自动生成
 	InsertProxyAddress(proxyAddress ProxyAddress) error
 
-	// RandomAvailableProxyAddresses 随机获取可用的代理地址，limit为获取个数上限
-	RandomAvailableProxyAddresses(limit int) ([]ProxyAddress, error)
+	// GetAvailableProxyAddress 获取一个可用的代理地址，会提供负载均衡的功能。protocol为代理协议
+	GetAvailableProxyAddress(protocol string) (ProxyAddress, error)
 
-	// RandomDetectedProxyAddresses 随机获取待检测的代理地址，limit为获取个数上限
-	RandomDetectedProxyAddresses(limit int) ([]ProxyAddress, error)
+	// GetDetectedProxyAddresses 获取待检测的代理地址。limit为获取个数上限
+	GetDetectedProxyAddresses(limit int) ([]ProxyAddress, error)
 
 	// PageProxyAddresses 分页查询代理地址。分别返回：总记录数、结果列表、可能发生的异常
 	PageProxyAddresses(pageNum, pageSize int) (int, []ProxyAddress, error)
@@ -20,8 +20,8 @@ type Storage interface {
 	// DeleteProxyAddress 删除代理地址
 	DeleteProxyAddress(id int) error
 
-	// FinishDetection 代理地址检测完成。success为是否检测成功
-	FinishDetection(id int, success bool) error
+	// FinishDetection 代理地址检测完成，如果检测失败次数超过阈值，会删除该代理地址。accept为是否通过检测
+	FinishDetection(id int, accept bool) error
 }
 
 // ProxyAddress 代理地址
